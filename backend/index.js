ffb05061be3b8ec4f930b5da1b0eaaef5d096dfc
saravanan-mca23 <yrsaravanan77@gmail.com>
2025-09-23 15:03:15 +0530

    cors update

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
